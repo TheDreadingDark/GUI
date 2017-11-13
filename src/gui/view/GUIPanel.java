8a2bb1a465c2f87441cfd6b7d16232a796244edc
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-11-13 12:58:47 -0700

    Added GUIPanel code and made a place for WindowBuilder code to go.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -9,10 +9,40 @@ import javax.swing.SpringLayout;
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
 	
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		firstButton = new JButton("Click on the button");
+		appLayout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
 	}
+	
+	/**
+	 * Used to add all components to the screen and install a layout manager.
+	 * Also set color hehe xd
+	 */
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.GREEN);
+		this.setLayout(appLayout);
+		this.add(firstButton);
+	}
+	
+	/**
+	 * Used to hold the constraint positioning for SpringLayout.
+	 * AKA a giant dumping ground for all the xxxx.putConstraint(....) lines.
+	 */
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.WEST, firstButton, 138, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, firstButton, -133, SpringLayout.SOUTH, this);
+	}
+	
 }
