2754f62e71338817fa6356a5f102d68e4cc31644
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-11-13 13:18:29 -0700

    Added change screen color code.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SpringLayout;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
@@ -21,6 +24,7 @@ public class GUIPanel extends JPanel
 		
 		setupPanel();
 		setupLayout();
+		setupListeners();
 	}
 	
 	/**
@@ -45,4 +49,23 @@ public class GUIPanel extends JPanel
 		appLayout.putConstraint(SpringLayout.SOUTH, firstButton, -133, SpringLayout.SOUTH, this);
 	}
 	
+	private void setupListeners()
+	{
+		firstButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				changeScreenColor();
+			}
+		});
+	}
+	
+	private void changeScreenColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+	}
 }
